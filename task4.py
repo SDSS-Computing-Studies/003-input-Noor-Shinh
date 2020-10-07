620f026c9d37bb0427d9cf8b5ce0d23a43c9741a
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 11:44:39 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -13,3 +13,13 @@
 #
 # Test output
 # input sides of 5 and 7 should give hypotenuse of 8.60232526704
+import math
+x=input("Enter value of side 1")
+y=input("Enter value of side 2")
+a=int(x)
+b=int(y)
+result1=math.pow(a,2)+ math.pow(b,2) 
+result2=math.sqrt(result1)
+result3=str(result2)
+response="Side 3 equals to "+ result3
+print(response)
