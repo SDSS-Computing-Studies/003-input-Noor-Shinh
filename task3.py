22e7bf20d90dbc36f129c8db6f794b412ae10882
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 11:44:06 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -12,3 +12,14 @@
 # solution for x
 #
 # test case: 5, 1, 11 should give x = 2
+import math 
+x=input("Enter value of a")
+y=input("Enter value of b")
+z=input("Enter value of c")
+a=float(x)
+b=float(y)
+c=float(z)
+result=(c-b)/a
+answer=str(result)
+response="x is equal to "+ answer
+print(answer)
