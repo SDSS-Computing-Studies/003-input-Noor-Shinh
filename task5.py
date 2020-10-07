865352206c7ee0fa39983594f2f8798814c0d130
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 11:45:00 -0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,3 +13,11 @@
 # Note: You will need to do some strange things with your cube root.
 # Remember that a cube root is the same as an exponent of 1/3, but
 # here you will need to do a power of 1.0/3 or something strange happens.
+import math
+x=input("Enter volume")
+v=float(x)
+result1=v*3/(4*math.pi)
+result2=math.pow(result1,1/3)
+result3=str(result2)
+response="Radius is " + result3
+print(response)
